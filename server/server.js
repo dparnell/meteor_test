2d9ad84f66a73be2059a06cfcaec76a0a8972035
Daniel Parnell <me@danielparnell.com>
2012-10-30 09:01:25 +1100

    Fixed the case where there is no button in the Buttons collection

--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
 Meteor.startup(function() {
 	// make sure there is a button in the buttons collection
 	var button = Buttons.findOne();
-	
+	var button_id;
+
 	if(button) {
+		button_id = button._id;
 		// Initialize the button
-		Buttons.update(button._id, { $set: { caption : 'Press Me' }} );
+		Buttons.update(button_id, { $set: { caption : 'Press Me' }} );
 	} else {
 		// create a button
-		Buttons.insert({ caption: 'Press Me', clicks : 0 });
+		button_id = Buttons.insert({ caption: 'Press Me', clicks : 0 });
 	}
 
 	Meteor.setInterval(function() {
-		Buttons.update(button._id, { $set: { caption : 'Press Me' }} );
+		Buttons.update(button_id, { $set: { caption : 'Press Me' }} );
 	}, 10000);
 });
 
